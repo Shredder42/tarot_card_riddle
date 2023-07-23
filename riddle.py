be52234ae64f2d479f3ca72184628574fb67e788
shredder <atbinder42@gmail.com>
2023-07-23 16:18:54 -0500

    project outline

---
+++ riddle.py
@@ -0,0 +1,27 @@
+'''
+program outline:
+* list of the cards 1-23
+* pick card from list
+* find factors function
+* check if card has any factors
+    * if so
+        * add card to player score and cards to player list
+        * add all factors to fate score
+        * remove all those cards from card list
+    * if not
+        * tell player to pick different card
+* check if any cards still have factors remaining
+    * if so
+        * pick a card
+    * if not
+        * game is over
+        * add remaining cards to fate score
+        * display socres and results
+
+
+
+
+'''
+
+def find_all_factors(card):
+    for
