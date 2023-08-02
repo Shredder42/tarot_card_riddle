f96fa1ea6261a01748bf197538654fb32784377f
shredder <atbinder42@gmail.com>
2023-08-01 21:52:44 -0500

    made game replayable

--- riddle.py
+++ riddle.py
@@ -15,7 +15,20 @@ program outline:
         * add remaining cards to fate score
         * display socres and results
 '''
+def ask_to_play_again():
+    play = input(('Would you like to play again? Type Y to play again. ')).lower()
+    if play == 'y':
+        return True
+    else:
+        return False
 
+def reset_game():
+    playable_cards = [i for i in range(1, 24)]
+    player_score = 0
+    player_cards = []
+    fate_score = 0
+    fate_cards = []
+    return playable_cards, player_score, player_cards, fate_score, fate_cards
 
 
 def pick_card(playable_cards):
@@ -23,7 +36,7 @@ def pick_card(playable_cards):
     choice = int(input('Pick a card from the list of playable cards: '))
     has_factor = determine_factors_present(choice, playable_cards)
     # print(determine_factors_present(choice, playable_cards))
-    while not has_factor:
+    while not has_factor or choice > 23:
         choice = int(input('You must pick a card from the playable cards list and the card must have a factor present. Pick again: '))
         has_factor = determine_factors_present(choice, playable_cards)
     return choice
@@ -84,32 +97,37 @@ def left_over_cards(playable_cards, fate_score, fate_cards):
 def game_result(player_score, player_cards, fate_score, fate_cards):
     if player_score > fate_score:
         print(f'You won! Your score: {player_score}: Fate score: {fate_score}')
+        if 2 in player_cards:
+            print('You claimed The High Priestess (2) and ended the curse!')
+        if 6 in player_cards:
+            print('You claimed the Lovers (6) and you will soon meet your true love!')
+        if 10 in player_cards:
+            print('You claimed the Wheel of Fortune and will soon receive unimaginable riches!')
     else:
         print(f'You lost! Your score: {player_score}: Fate score: {fate_score}')
-    print(f'You finished with these cards {player_cards}')
-    print(f'Fate finished with these cards: {fate_cards}')
+        print(f'You finished with these cards {player_cards}')
+        print(f'Fate finished with these cards: {fate_cards}')
 
 
 
 def main():
-    playable_cards = [i for i in range(1, 24)]
-    player_score = 0
-    player_cards = []
-    fate_score = 0
-    fate_cards = []
-    factors_remain = True
-    while factors_remain:
-        print_playable_cards(playable_cards)
-        card = pick_card(playable_cards)
-        factors_list = find_factors(card)
-        player_score, player_cards = update_player_score_and_cards(card, player_score, player_cards)
-        fate_score, fate_cards = update_fate_cards_and_score(factors_list, fate_score, fate_cards)
-        print(f'Player score: {player_score}. Player cards: {player_cards}')
-        print(f'Fate score: {fate_score}. Fate cards: {fate_cards}')
-        playable_cards = remove_playable_cards(playable_cards, card, factors_list)
-        factors_remain = determine_if_factors_remaining(playable_cards)
-    fate_score, fate_cards = left_over_cards(playable_cards, fate_score, fate_cards)
-    game_result(player_score, player_cards, fate_score, fate_cards)
+    play = True
+    while play:
+        playable_cards, player_score, player_cards, fate_score, fate_cards = reset_game()
+        factors_remain = True
+        while factors_remain:
+            print_playable_cards(playable_cards)
+            card = pick_card(playable_cards)
+            factors_list = find_factors(card)
+            player_score, player_cards = update_player_score_and_cards(card, player_score, player_cards)
+            fate_score, fate_cards = update_fate_cards_and_score(factors_list, fate_score, fate_cards)
+            print(f'Player score: {player_score}. Player cards: {player_cards}')
+            print(f'Fate score: {fate_score}. Fate cards: {fate_cards}')
+            playable_cards = remove_playable_cards(playable_cards, card, factors_list)
+            factors_remain = determine_if_factors_remaining(playable_cards)
+        fate_score, fate_cards = left_over_cards(playable_cards, fate_score, fate_cards)
+        game_result(player_score, player_cards, fate_score, fate_cards)
+        play = ask_to_play_again()
 
 
 if __name__ == '__main__':
